863efb6f9d9b9575cb6abd89bff5e3e7f38e07a8
ABEL-1010 <abeldemoz4@gmail.com>
2026-02-03 08:28:21 +0300

    user home page

---
+++ frontend/src/pages/user/UserHomePage.jsx
@@ -0,0 +1,10 @@
+const UserHomePage = () => {
+
+    return (
+        <div className="min-h-screen bg-white flex flex-col items-center justify-center text-center px-4 relative">
+            <h1 className="text-black text-4xl md:text-6xl font-extrabold tracking-wide">
+             GET INFORMED
+            </h1>
+        </div>
+    )
+}
