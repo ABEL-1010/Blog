3a7fcde62b8555117c81ef0f073ef6c3b503ef00
ABEL-1010 <abeldemoz4@gmail.com>
2026-02-03 08:43:28 +0300

    fixing User home page

--- frontend/src/pages/user/UserHomePage.jsx
+++ frontend/src/pages/user/UserHomePage.jsx
@@ -1,4 +1,4 @@
-const UserHomePage = () => {
+export default function UserHomePage() {
 
     return (
         <div className="min-h-screen bg-white flex flex-col items-center justify-center text-center px-4 relative">
@@ -8,6 +8,13 @@ const UserHomePage = () => {
             <h1 className="text-black text-4xl md:text-6xl font-extrabold tracking-wide mt-2">
                 GET INSPIRED
             </h1>
+            <div className="w-64 h-1 bg-purple-700 my-6"></div>
+            <p className="text-gray-700 text-lg md:text-xl max-w-xl">
+                Stories Curated For You
+            </p>
+            <button className="mt-6 bg-purple-700 hover:bg-purple-700 transition text-white px-10 py-3 rounded-md font-semibold text-lg">
+                Write
+            </button>
         </div>
     )
 }
